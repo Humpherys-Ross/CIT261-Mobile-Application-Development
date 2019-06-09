de6e3c33d85cb1804c7a4b9d5fa42751fe8cc621
Humpherys-Ross <49924165+Humpherys-Ross@users.noreply.github.com>
2019-06-08 21:46:15 -0400

    Added jQuery and array with css code

--- Java.js
+++ Java.js
@@ -1,5 +1,21 @@
-$(function() {
-  $(".Shelf").click(function() {
-    $(this).toggleClass("different");
+$(document).ready(function() {
+  var ProductArray = [
+    "Eggs",
+    "Cheese",
+    "Milk",
+    "Bread",
+    "Cereal",
+    "Chocolate",
+    "Ham",
+    "ChexMix",
+    "Oranges",
+    "Tomatoes"
+  ];
+  for (var i = 0; i < ProductArray.length; i++) {
+    var div = document.getElementById("Pos" + (i + 1));
+    div.innerHTML = ProductArray[i];
+  }
+  $("div").click(function(event) {
+    $(event.target).toggleClass("minus");
   });
 });
