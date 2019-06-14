b01d605e7672368fb6854c7e77d4c6e1b331690e
Humpherys-Ross <49924165+Humpherys-Ross@users.noreply.github.com>
2019-06-14 18:15:49 -0400

    Removed jQuery, changed some Divs and classes

--- Java.js
+++ Java.js
@@ -1,21 +1,16 @@
-$(document).ready(function() {
-  var ProductArray = [
-    "Eggs",
-    "Cheese",
-    "Milk",
-    "Bread",
-    "Cereal",
-    "Chocolate",
-    "Ham",
-    "ChexMix",
-    "Oranges",
-    "Tomatoes"
-  ];
-  for (var i = 0; i < ProductArray.length; i++) {
-    var div = document.getElementById("Pos" + (i + 1));
-    div.innerHTML = ProductArray[i];
-  }
-  $("div").click(function(event) {
-    $(event.target).toggleClass("minus");
-  });
-});
+var ProductArray = [
+  "Eggs",
+  "Cheese",
+  "Milk",
+  "Bread",
+  "Cereal",
+  "Chocolate",
+  "Ham",
+  "ChexMix",
+  "Oranges",
+  "Tomatoes"
+];
+for (var i = 0; i < ProductArray.length; i++) {
+  var div = document.getElementById("Pos" + (i + 1));
+  div.innerHTML = ProductArray[i];
+}
