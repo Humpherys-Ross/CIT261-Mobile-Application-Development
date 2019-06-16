9bc8ea220c9f031a5b291e12d1077ebde40de05b
Humpherys-Ross <49924165+Humpherys-Ross@users.noreply.github.com>
2019-06-16 18:16:27 -0400

    Had a friend help rework my code and streamline it

--- Java.js
+++ Java.js
@@ -1,4 +1,4 @@
-var ProductArray = [
+var productArray = [
   "Eggs",
   "Cheese",
   "Milk",
@@ -10,7 +10,47 @@ var ProductArray = [
   "Oranges",
   "Tomatoes"
 ];
-for (var i = 0; i < ProductArray.length; i++) {
-  var div = document.getElementById("Pos" + (i + 1));
-  div.innerHTML = ProductArray[i];
-}
+
+
+var targetId = "board-container";
+
+/* <div>
+<button onclick="RmvPrd()" class="fas fa-minus"></button>
+<div id="Pos1"></div>
+</div> */
+
+var boardContainer = document.getElementById("board-container");
+
+productArray.forEach( (value, i, a) => {
+  var shelfItem = document.createElement("div");
+  shelfItem.className = "fas fa-red"
+  shelfItem.setAttribute("count", "0")
+  
+  var button = document.createElement("button");
+  button.onclick = () => { 
+    console.log(`clicked button ${value}`)
+    
+    // increment
+     var count = parseInt(shelfItem.getAttribute("count")) + 1;
+     shelfItem.setAttribute("count", count);
+
+     console.log(`count for ${value} set to ${count}`);
+
+    if(count > 0) shelfItem.className = "fas fa-green";
+  };
+  button.className = "fas fa-minus"
+
+  var shelfContent = document.createElement("div");
+  shelfContent.innerHTML = value
+
+  shelfItem.appendChild(button);
+  shelfItem.appendChild(shelfContent);
+
+  boardContainer.appendChild(shelfItem);
+});
+
+
+// for (var i = 1; i < productArray.length; i++) {
+//   var div = document.getElementById("Pos" + i);
+//   div.innerHTML = ProductArray[i];
+// }
